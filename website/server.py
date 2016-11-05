f46510fa62c13a846552cb62e289d08aae3e1c3c
rmaheshkumarblr <rmaheshkumarblr@gmail.com>
2016-11-05 01:31:43 -0600

    Updated csv file with Country code - To be used for Choropleth

--- website/server.py
+++ website/server.py
@@ -1,20 +1,28 @@
 from flask import Flask, render_template , jsonify ,request
 from flask_bootstrap import Bootstrap
 from collections import Counter
+import pycountry
 import csv
 
 
 app = Flask(__name__,static_url_path='/static')
 Bootstrap(app)
 
+
 @app.route("/")
 def index():
+    return render_template('base.html')
+
+
+@app.route("/smallMultiple")
+def smallMultiple():
     return render_template('smallMultiple.html')
 
 
 @app.route("/barChart")
 def barChart():
     return render_template('barChart.html')
+
 
 @app.route("/survivorsVsFlightType")
 def survivorsVsFlightType():
@@ -31,8 +39,6 @@ def survivorsVsFlightType():
         return jsonify(result)
 
 
-
-
 @app.route("/yearVsAccidents")
 def yearVsAccidents():
     result = []
@@ -42,7 +48,7 @@ def yearVsAccidents():
         for row in dataReader:
             if row[9] == "Aboard":
                 continue
-            if len(row) == 15:
+            if len(row) == 15 and row[13] != "?":
                 year = row[0].split(",")[1]
                 location = row[2]
                 operator = row[3]
@@ -88,6 +94,65 @@ def yearVsAccidents():
 # def hello1():
 #     return app.send_static_file('sales.csv')
 
+
+@app.route("/yearVsAccidentsArea")
+def yearVsAccidentsArea():
+
+    countries = {}
+    for country in pycountry.countries:
+        countries[country.name] = country.alpha2
+    result = []
+
+    with open('static/data/data.csv', 'rb') as csvfile:
+        dataReader = csv.reader(csvfile, delimiter=',')
+        for row in dataReader:
+            if row[9] == "Aboard":
+                continue
+            if row[2] != "?":
+                if "," in row[2]:
+                    print "Part of the Name: " , row[2].split(",")[1].strip() , countries.get(row[2].split(",")[1].strip(), 'Unknown code')
+                    if (countries.get(row[2].split(",")[1].strip(), 'Unknown code') == 'Unknown code'):
+                        result.append(row[2].split(",")[1].strip())
+                else:
+                    print "Entire Name: " , row[2].strip() , countries.get(row[2].strip(), 'Unknown code') 
+                    if ( countries.get(row[2].strip(), 'Unknown code') == 'Unknown code'):
+                        result.append(row[2].strip())
+
+        # print countries
+        print len(set(result))
+        #     if len(row) == 15:
+        #         year = row[0].split(",")[1]
+        #         location = row[2]
+        #         operator = row[3]
+        #         aircraftType = row[6]
+        #         aboard = row[9]
+        #         fatalities = row[10]
+        #         latitude = row[13]
+        #         longitude = row[14]
+        #         summary = row[12]
+
+        #         if(fatalities == "?"):
+        #             fatalities = 0
+        #         if(int(fatalities) == 0 ):
+        #             fatalitiesNameBasedOnCount = 'nofatalities'
+        #         else:
+        #             fatalitiesNameBasedOnCount = 'fatalities'
+                 
+        #         content = {
+        #             "name": operator,
+        #             "year": year,
+        #             "location": location,
+        #             "operator": operator,
+        #             "aircraftType": aircraftType,
+        #             "aboard": aboard,
+        #             "fatalities": fatalities,
+        #             "latitude": latitude,
+        #             "longitude": longitude,
+        #             "fillKey" : fatalitiesNameBasedOnCount,
+        #             "summary": summary
+        #         }
+        #         result.append(content)
+        # return jsonify(result)
 
 if __name__ == "__main__":
     app.run(port=8001,debug=True)
